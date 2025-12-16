2d51d3efe455089b5d99bc11a0fef646065d9b02
Jim232-cmd <ivanmontesinos087@gmail.com>
2025-12-16 16:18:00 -0600

    print_ivan

---
+++ ivan.py
@@ -0,0 +1,4 @@
+print("Hola mi nombre es: Ivan Montesinos Gutierrez")
+print("Mi domicilio es:Piedra grande")
+print("Mi color favorito es: El color amarillo")
+print("Mis hobbies favoritos son:Jugar videojuegos y tocar la guitarra")
