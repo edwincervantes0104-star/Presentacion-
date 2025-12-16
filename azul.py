6420ba12e7bcc2df3a0d6131eb866e96fd14455b
edwincervantes0104-star <edwincervantes0104@gmail.com@gmail.com>
2025-12-16 17:53:44 -0600

    print_azul

---
+++ azul.py
@@ -0,0 +1,4 @@
+print("Nombre completo: Mildred Azul Cárdenas López")
+print("Domicilio: San Ramón, Huizquilucan, Estado de México, camino a las carreras")
+print("Color favorito: Verde y morado")
+print("Hobbies: Escuchar música, dormir y nadar")
