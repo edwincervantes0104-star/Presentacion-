0d2328c9e34651933e7819ac332f9e5e89c3582e
edwincervantes0104-star <edwincervantes0104@gmail.com@gmail.com>
2025-12-16 16:38:22 -0600

    print_cervantes

--- cervantes.py
+++ cervantes.py
@@ -1,4 +1,4 @@
 print("Hola mi nombre es: Edwin cervantes celedonio")
 print("Mi domicilio es:San miguel ")
-print("Mi color favorito es: El color blanco y azul")
+print("Mi color favorito es: El color blanco y cian (color azul mas clarito)")
 print("Mis hobbies favoritos son: Aprender cosas nuevas y salir a dar una vuelta")
