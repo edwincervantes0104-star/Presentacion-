a482d4fef84f38f1f158b0d990c597271e6ebb8a
DAG18575-SVG <dag18575@gmail.com>
2025-12-16 18:17:05 -0600

    omar

---
+++ omar.py
@@ -0,0 +1,4 @@
+print("Hola mi nombre es: Omar Reyes Tomate")
+print("Mi domicilio es:San cristobal ")
+print("Mi color favorito es: El azul o mejor dicho el cyan el naranja y el morado")
+print("Mis hobbies favoritos son: jugar videojuegos dibujar aunque no soy tan bueno y tambien jugar basketball")
